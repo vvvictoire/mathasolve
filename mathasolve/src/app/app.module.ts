31d2cc87e6ebeb8b7d3a2d53821bb8a245dcb63c
Victoire <vvictoire@pm.me>
2022-04-16 19:39:25 +0200

    display the scores

--- mathasolve/src/app/app.module.ts
+++ mathasolve/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NumberDisplayComponent } from './number-display/number-display.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NumberDisplayComponent
+    NumberDisplayComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
